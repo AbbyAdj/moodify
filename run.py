7b50510b7621213558fe164e46bb30b42d4857a2
AbbyAdj <abbyadjei9@gmail.com>
2025-06-16 23:56:46 +0100

    update run

--- run.py
+++ run.py
@@ -2,8 +2,9 @@ from src import app
 from waitress import serve
 
 if __name__ == "__main__":
-    # try:
-    #     serve(app, host="0.0.0.0", port=8080)
-    # except Exception as e:
-    #     print("ERROR:", e)
-    app.run(debug=True)
+    try:
+        serve(app, host="0.0.0.0", port=8080)
+    except Exception as e:
+        print("ERROR:", e)
+    # app.run(debug=True)
+
